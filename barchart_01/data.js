7329f60272b06b34be56e6073b86efbe3dc6ad5e
Carlos Niermeier <info@mindcraft-design.com>
2017-07-10 18:37:35 +0200

    Revert "Revert "kommentiert""
    This reverts commit 2d1e24908a50298adc3d45a6a0af70b74f00eb2d.

--- barchart_01/data.js
+++ barchart_01/data.js
@@ -5,8 +5,11 @@ var height = 500;
 //Tage untereinander, muss translate angepasst werden (Bsp. Tag1 und Tag2)
 var paddingTag = 130;
 
+// Kreise in Y-Achse zueinander
 var kreisZuKreis = 12;
-//Abstand zum zweiten Teil der jweiligen Schichtart (Bsp. 1. Teil früh zu 2. Teil früh)
+
+//Abstand zum zweiten Teil der jweiligen Schichtart (Bsp. 1. Teil früh zu 2. Teil früh) 
+//Auf der X-Achse
 var zweiteHälfte = 12;
 
 // Abstand von einer Schichtart zu anderen (Bsp.: Früh zu Mitte)
